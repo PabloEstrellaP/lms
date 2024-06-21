80e0f377edf3bc0a9f105631d856f5bb0af6137c
PabloEstrellaP <48409673+PabloEstrellaP@users.noreply.github.com>
2024-06-21 13:59:02 -0600

    Update purchasedCourse.js

--- routes/purchasedCourse.js
+++ routes/purchasedCourse.js
@@ -4,13 +4,18 @@ import { verifyJWT } from '../helpers/JWT.js'
 
 const routerUser = Router()
 
-routerUser.get('/', [verifyJWT], getPurchasedCourse)
-routerUser.get('/:id', [verifyJWT], getPurchasedCourseByUserIdAndCourseId)
+// routerUser.get('/', [verifyJWT], getPurchasedCourse)
+routerUser.get('/', getPurchasedCourse)
+// routerUser.get('/:id', [verifyJWT], getPurchasedCourseByUserIdAndCourseId)
+routerUser.get('/:id', getPurchasedCourseByUserIdAndCourseId)
 
-routerUser.put('/:id', [verifyJWT], putPurchasedCourse)
+// routerUser.put('/:id', [verifyJWT], putPurchasedCourse)
+routerUser.put('/:id', putPurchasedCourse)
 
-routerUser.post('/', [verifyJWT], postPurchasedCourse)
+// routerUser.post('/', [verifyJWT], postPurchasedCourse)
+routerUser.post('/', postPurchasedCourse)
 
-routerUser.delete('/:id', [verifyJWT], deletePurchasedCourse)
+// routerUser.delete('/:id', [verifyJWT], deletePurchasedCourse)
+routerUser.delete('/:id', deletePurchasedCourse)
 
 export default routerUser
