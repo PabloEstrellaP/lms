c2f77e17b80259a1a75e624480004cceb0884c31
PabloEstrellaP <48409673+PabloEstrellaP@users.noreply.github.com>
2024-06-21 13:58:06 -0600

    Update courseProgress.js

--- routes/courseProgress.js
+++ routes/courseProgress.js
@@ -4,13 +4,19 @@ import { verifyJWT } from '../helpers/JWT.js'
 
 const routerUser = Router()
 
-routerUser.get('/', [verifyJWT], getCourseProgress)
-routerUser.get('/:id', [verifyJWT], getCourseProgressByPurchasedCourseId)
+// routerUser.get('/', [verifyJWT], getCourseProgress)
+routerUser.get('/', getCourseProgress)
+// routerUser.get('/:id', [verifyJWT], getCourseProgressByPurchasedCourseId)
+routerUser.get('/:id', getCourseProgressByPurchasedCourseId)
 
-routerUser.put('/:id', [verifyJWT], putCourseProgress)
+// routerUser.put('/:id', [verifyJWT], putCourseProgress)
+routerUser.put('/:id', putCourseProgress)
 
-routerUser.post('/', [verifyJWT], postCourseProgress)
+// routerUser.post('/', [verifyJWT], postCourseProgress)
+routerUser.post('/', postCourseProgress)
 
-routerUser.delete('/:id', [verifyJWT], deleteCourseProgress)
+// routerUser.delete('/:id', [verifyJWT], deleteCourseProgress)
+routerUser.delete('/:id', deleteCourseProgress)
+
 
 export default routerUser
